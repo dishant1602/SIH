fc441ed39b769929d1935af92c997aa5e6aa581b
dishant1602 <ddshah02@gmail.com>
2022-03-13 20:47:58 +0530

    Fifth commit

--- app/src/main/java/com/example/sih/HomePage.java
+++ app/src/main/java/com/example/sih/HomePage.java
@@ -1,92 +1,90 @@
 package com.example.sih;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class HomePage extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-    CardView haridwarcard, katarmalcard, baijnathcard, pbtcard, binsarcard, jageshwarcard, dandeshwarcard, baleshwarcard, mallacard, banasurcard;
-    CardView rajbhungacard, pithoragarhcard, askotcard, almoracard, simtolacard, kasarcard, mayawaticard, nainicard, damyanticard, satcard, bhimcard, khurpacard, naukuchiyacard;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class HomePage extends AppCompatActivity{
+    TextView yamdist, gangdist, kedardist, badridist;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home_page);
-
-        haridwarcard=findViewById(R.id.haridwarcard);
-        baijnathcard=findViewById(R.id.baijnathcard);
-        katarmalcard=findViewById(R.id.katarmalcard);
-        pbtcard=findViewById(R.id.pbtcard);
-        binsarcard=findViewById(R.id.binsarcard);
-        jageshwarcard=findViewById(R.id.jageshwarcard);
-        dandeshwarcard=findViewById(R.id.dandeshwarcard);
-        baleshwarcard=findViewById(R.id.baleshwarcard);
-        mallacard=findViewById(R.id.mallacard);
-        banasurcard=findViewById(R.id.banasurcard);
-        rajbhungacard=findViewById(R.id.rajbhungacard);
-        pithoragarhcard=findViewById(R.id.pithoragarhcard);
-        askotcard=findViewById(R.id.askotcard);
-        almoracard=findViewById(R.id.almoracard);
-        simtolacard=findViewById(R.id.simtolacard);
-        kasarcard=findViewById(R.id.kasarcard);
-        mayawaticard=findViewById(R.id.mayawaticard);
-        nainicard=findViewById(R.id.nainicard);
-        damyanticard=findViewById(R.id.damyanticard);
-        satcard=findViewById(R.id.satcard);
-        bhimcard=findViewById(R.id.bhimcard);
-        khurpacard=findViewById(R.id.khurpacard);
-        naukuchiyacard=findViewById(R.id.naukuchiyacard);
-
-        haridwarcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Haridwar\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        baijnathcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Baijnath\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        katarmalcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Katarmal\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        pbtcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Patal Bhuvaneshwar\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        binsarcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Binsar Mahadev\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        jageshwarcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Jageshwar\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        dandeshwarcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Dandeshwar\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        baleshwarcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Baleshwar\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        mallacard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Malla Mahal\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        banasurcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Banasur Fort\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        rajbhungacard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Rajbhunga Fort\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        pithoragarhcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Pithoragarh Fort\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        askotcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Askot\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        almoracard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Almora\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        simtolacard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Simtola\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        kasarcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Kasar Devi\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        mayawaticard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Mayawati\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        nainicard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Naini Lake\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        damyanticard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Nal Damayanti Tal\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        satcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Sat Tal\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        bhimcard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Bhim Tal\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        khurpacard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Khurpa Tal\uD83D\uDE42", Toast.LENGTH_SHORT).show());
-
-        naukuchiyacard.setOnClickListener(view -> Toast.makeText(HomePage.this, "Will soon add more info about Naukuchiya Tal\uD83D\uDE42", Toast.LENGTH_SHORT).show());
+        yamdist = findViewById(R.id.yamdist);
+        gangdist = findViewById(R.id.gangdist);
+        kedardist = findViewById(R.id.kedardist);
+        badridist = findViewById(R.id.badridist);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if(ActivityCompat.checkSelfPermission(HomePage.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+                    if(location != null){
+                        try {
+                            Geocoder geocoder = new Geocoder(HomePage.this, Locale.getDefault());
+                            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                            Double j = addresses.get(0).getLatitude();
+                            Double k = addresses.get(0).getLongitude();
+                            Location currentLocation = new Location("locationA");
+                            currentLocation.setLatitude(17.372102);
+                            currentLocation.setLongitude(78.484196);
+                            Location yamunotri = new Location("locationB");
+                            yamunotri.setLatitude(31.01450);
+                            yamunotri.setLongitude(78.46044);
+                            double dist1 = currentLocation.distanceTo(yamunotri);
+                            DecimalFormat df = new DecimalFormat();
+                            df.setMaximumFractionDigits(2);
+                            String exact_yamunotri = df.format((dist1/1000));
+                            yamdist.setText(exact_yamunotri+" KM");
+                            Location gangotri = new Location("locationC");
+                            gangotri.setLatitude(31.00081);
+                            gangotri.setLongitude(78.92944);
+                            double dist2 = currentLocation.distanceTo(gangotri);
+                            String exact_gangotri = df.format((dist2/1000));
+                            gangdist.setText(exact_gangotri+" KM");
+                            Location kedarnath = new Location("locationD");
+                            kedarnath.setLatitude(30.76117);
+                            kedarnath.setLongitude(79.07015);
+                            double dist3 = currentLocation.distanceTo(kedarnath);
+                            String exact_kedarnath = df.format((dist3/1000));
+                            kedardist.setText(exact_kedarnath+" KM");
+                            Location badrinath = new Location("locationE");
+                            badrinath.setLatitude(30.76117);
+                            badrinath.setLongitude(79.07015);
+                            double dist4 = currentLocation.distanceTo(kedarnath);
+                            String exact_badrinath = df.format((dist4/1000));
+                            kedardist.setText(exact_badrinath+" KM");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        }
+        else{
+            ActivityCompat.requestPermissions(HomePage.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+        }
     }
 }
