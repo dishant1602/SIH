358292cc3b40fffc0b0cc10b8ca25ef14890948c
Dishant1602 <dishantsakhida@gmail.com>
2022-10-27 23:20:01 +0530

    Major Weather Page Update

--- app/src/main/java/com/example/sih/WeatherPage.java
+++ app/src/main/java/com/example/sih/WeatherPage.java
@@ -1,11 +1,14 @@
 package com.example.sih;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
-import android.location.LocationManager;
-import android.location.LocationListener;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -24,18 +27,24 @@ import cz.msebera.android.httpclient.Header;
 public class WeatherPage extends AppCompatActivity {
     final String APP_ID = "dab3af44de7d24ae7ff86549334e45bd";
     final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
-    TextView x_curr, x_max, x_min, x_wind, textView17;
-    Button yam_but, gangotri_button, kedarnath_button, badrinath_button, hc_button, gaurikund_button, nt_button, gaumukh_button, rnp_button, fri_button, hkp_button, tapkeshwar_button, manasa_button, teramanzil_button, vyas_button, robber_button, sd_button, skiing_button, gqn_button, lt_button, geh_button, close_weathinfo;
+    TextView textView9, textView11, textView12, textView14, textView16, textView19, textView20, textView21, textView22;
+    CardView yam_but, gangotri_button, kedarnath_button, badrinath_button, hc_button, gaurikund_button, nt_button, gaumukh_button, rnp_button, fri_button, hkp_button, tapkeshwar_button, manasa_button, teramanzil_button, vyas_button, robber_button, sd_button, skiing_button, gqn_button, lt_button, geh_button;
     Double Latitude=null;
     Double Longitude=null;
     Boolean sidecond=false;
-    LinearLayout weatherdisp, sidemenu_weatherpage, tncwop, faqwop, aboutuswop, precwop, disasterwop;
-    ImageView weatherpage_home, weatherpage_menu, weatherpage_hotel;
+    LinearLayout sidemenu_weatherpage, tncwop, faqwop, aboutuswop, precwop, disasterwop;
+    ImageView weatherpage_home, weatherpage_menu, weatherpage_hotel, imageView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_weather_page);
+        textView14=findViewById(R.id.textView14);
+        textView16=findViewById(R.id.textView16);
+        textView19=findViewById(R.id.textView19);
+        textView20=findViewById(R.id.textView20);
+        textView21=findViewById(R.id.textView21);
+        textView22=findViewById(R.id.textView22);
         sidemenu_weatherpage=findViewById(R.id.sidemenu_weatherpage);
         tncwop=findViewById(R.id.tncwop);
         aboutuswop=findViewById(R.id.aboutuswop);
@@ -45,13 +54,6 @@ public class WeatherPage extends AppCompatActivity {
         weatherpage_home=findViewById(R.id.weatherpage_home);
         weatherpage_hotel=findViewById(R.id.weatherpage_hotel);
         weatherpage_menu=findViewById(R.id.weatherpage_menu);
-        x_curr=findViewById(R.id.x_curr);
-        weatherdisp=findViewById(R.id.weatherdisp);
-        close_weathinfo=findViewById(R.id.close_weathinfo);
-        textView17=findViewById(R.id.textView17);
-        x_max=findViewById(R.id.x_max);
-        x_min=findViewById(R.id.x_min);
-        x_wind=findViewById(R.id.x_wind);
         yam_but=findViewById(R.id.yam_but);
         gangotri_button=findViewById(R.id.gangotri_button);
         kedarnath_button=findViewById(R.id.kedarnath_button);
@@ -73,7 +75,10 @@ public class WeatherPage extends AppCompatActivity {
         gqn_button=findViewById(R.id.gqn_button);
         lt_button=findViewById(R.id.lt_button);
         geh_button=findViewById(R.id.geh_button);
-        weatherdisp.setVisibility(View.GONE);
+        textView9=findViewById(R.id.textView9);
+        textView11=findViewById(R.id.textView11);
+        textView12=findViewById(R.id.textView12);
+        imageView2=findViewById(R.id.imageView2);
         weatherpage_home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -143,153 +148,152 @@ public class WeatherPage extends AppCompatActivity {
                 startActivity(disfromweather);
             }
         });
-        close_weathinfo.setOnClickListener(view -> weatherdisp.setVisibility(View.GONE));
         yam_but.setOnClickListener(view -> {
-            textView17.setText("Yamunotri");
+            textView11.setText("Yamunotri");
             Latitude=31.01450;
             Longitude=78.46044;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(yam_but);
         });
         gangotri_button.setOnClickListener(view -> {
-            textView17.setText("Gangotri");
+            textView11.setText("Gangotri");
             Latitude=31.00081;
             Longitude=78.92944;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(gangotri_button);
         });
         kedarnath_button.setOnClickListener(view -> {
-            textView17.setText("Kedarnath");
+            textView11.setText("Kedarnath");
             Latitude=30.76117;
             Longitude=79.07015;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(kedarnath_button);
         });
         badrinath_button.setOnClickListener(view -> {
-            textView17.setText("Badrinath");
+            textView11.setText("Badrinath");
             Latitude=30.7433;
             Longitude=79.4938;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(badrinath_button);
         });
         hc_button.setOnClickListener(view -> {
-            textView17.setText("Hanuman Chatti -  Janki Chatti");
+            textView11.setText("Hanuman Chatti -  Janki Chatti");
             Latitude=30.9324;
             Longitude=78.3992;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(hc_button);
         });
         nt_button.setOnClickListener(view -> {
-            textView17.setText("Nag Tibba");
+            textView11.setText("Nag Tibba");
             Latitude=30.58925;
             Longitude=78.13903;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(nt_button);
         });
         gaurikund_button.setOnClickListener(view -> {
-            textView17.setText("Gaurikund - Kedarnath");
+            textView11.setText("Gaurikund - Kedarnath");
             Latitude=30.65601;
             Longitude=79.02821;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(gaurikund_button);
         });
         gaumukh_button.setOnClickListener(view -> {
-            textView17.setText("Gaumukh Trek");
+            textView11.setText("Gaumukh Trek");
             Latitude=30.75402;
             Longitude=78.44484;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(gaumukh_button);
         });
         rnp_button.setOnClickListener(view -> {
-            textView17.setText("Rajaji National Museum");
+            textView11.setText("Rajaji National Museum");
             Latitude=29.9927;
             Longitude=78.2437;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(rnp_button);
         });
         fri_button.setOnClickListener(view -> {
-            textView17.setText("FRI Museum");
+            textView11.setText("FRI Museum");
             Latitude=30.3438;
             Longitude=77.9978;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(fri_button);
         });
         hkp_button.setOnClickListener(view -> {
-            textView17.setText("Har ki Pauri");
+            textView11.setText("Har ki Pauri");
             Latitude=29.9567;
             Longitude=78.1710;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(hkp_button);
         });
         teramanzil_button.setOnClickListener(view -> {
-            textView17.setText("Tera Manzil Mandir");
+            textView11.setText("Tera Manzil Mandir");
             Latitude=30.12659;
             Longitude=78.33076;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(teramanzil_button);
         });
         manasa_button.setOnClickListener(view -> {
-            textView17.setText("Manasa Devi Temple");
+            textView11.setText("Manasa Devi Temple");
             Latitude=29.9579;
             Longitude=78.1653;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(manasa_button);
         });
         tapkeshwar_button.setOnClickListener(view -> {
-            textView17.setText("Tapkeshwar Temple");
+            textView11.setText("Tapkeshwar Temple");
             Latitude=30.35748;
             Longitude=78.01666;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(tapkeshwar_button);
         });
         vyas_button.setOnClickListener(view -> {
-            textView17.setText("Vyas Gufa");
+            textView11.setText("Vyas Gufa");
             Latitude=30.7743;
             Longitude=79.4949;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(vyas_button);
         });
         sd_button.setOnClickListener(view -> {
-            textView17.setText("Sahastradhara");
+            textView11.setText("Sahastradhara");
             Latitude=30.3884;
             Longitude=78.1294;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(sd_button);
         });
         robber_button.setOnClickListener(view -> {
-            textView17.setText("Robbers Cave");
+            textView11.setText("Robbers Cave");
             Latitude=30.3766;
             Longitude=78.0612;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(robber_button);
         });
         skiing_button.setOnClickListener(view -> {
-            textView17.setText("Auli");
+            textView11.setText("Auli");
             Latitude=30.5305;
             Longitude=79.5685;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(skiing_button);
         });
         geh_button.setOnClickListener(view -> {
-            textView17.setText("George Everest House");
+            textView11.setText("George Everest House");
             Latitude=30.4591;
             Longitude=78.0229;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(geh_button);
         });
         gqn_button.setOnClickListener(view -> {
-            textView17.setText("Garhwal Queen Nilkanth");
+            textView11.setText("Garhwal Queen Nilkanth");
             Latitude=30.08092;
             Longitude=78.34092;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(gqn_button);
         });
         lt_button.setOnClickListener(view -> {
-            textView17.setText("Lal Tibba");
+            textView11.setText("Lal Tibba");
             Latitude=30.4667;
             Longitude=78.0950;
-            weatherdisp.setVisibility(View.VISIBLE);
             getweatherforxloc();
+            cardbackgroundcolor(lt_button);
         });
     }
     private void getweatherforxloc() {
@@ -306,6 +310,7 @@ public class WeatherPage extends AppCompatActivity {
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(WEATHER_URL,params,new JsonHttpResponseHandler()
         {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 //super.onSuccess(statusCode, headers, response);
@@ -324,10 +329,42 @@ public class WeatherPage extends AppCompatActivity {
     }
     private  void updateUI(weatherData weather){
 
-        x_curr.setText(weather.getmTemperature());
-        x_min.setText(weather.getMinTemp());
-        x_max.setText(weather.getMaxTemp());
-        x_wind.setText(weather.getwindSpeed());
+        textView9.setText(weather.getmTemperature());
+        textView12.setText(weather.getMaint());
+        Log.d("abc",weather.getMicon());
+        int resourID=getResources().getIdentifier(weather.getMicon(),"drawable",getPackageName());
+        imageView2.setImageResource(resourID);
+        textView14.setText(weather.getwindSpeed());
+        textView16.setText(weather.getMhumidity());
+        textView19.setText(weather.getmClouds());
+        textView20.setText(weather.getSunRise());
+        textView21.setText(weather.getmPressure());
+        textView22.setText(weather.getSunSet());
 
+    }
+
+    private void cardbackgroundcolor(CardView h){
+        yam_but.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        gangotri_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        kedarnath_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        badrinath_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        hc_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        gaurikund_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        nt_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        gaumukh_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        rnp_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        fri_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        hkp_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        tapkeshwar_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        manasa_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        teramanzil_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        vyas_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        robber_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        sd_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        skiing_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        gqn_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        lt_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        geh_button.setCardBackgroundColor(Color.parseColor("#F8FAFE"));
+        h.setCardBackgroundColor(Color.parseColor("#2ABAFF"));
     }
 }
--- app/src/main/java/com/example/sih/weatherData.java
+++ app/src/main/java/com/example/sih/weatherData.java
@@ -1,30 +1,55 @@
 package com.example.sih;
 
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class weatherData {
     private String mTemperature;
-    private String maxTemp;
-    private String minTemp;
+    private String sunRise,micon,sunSet;
+    private String maint;
+    private String mHumidity, mClouds, mPressure;
     private double windSpeed;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public static weatherData fromJson(JSONObject jsonObject)
     {
 
         try
         {
             weatherData weatherD=new weatherData();
+            //Temperature
             double tempResult=jsonObject.getJSONObject("main").getDouble("temp")-273.15;
-            double maxResult=jsonObject.getJSONObject("main").getDouble("temp_max")-273.15;
-            double minResult=jsonObject.getJSONObject("main").getDouble("temp_min")-273.15;
-            weatherD.windSpeed=jsonObject.getJSONObject("wind").getDouble("speed");
             int roundedValue=(int)Math.rint(tempResult);
-            int overcome=(int)Math.rint(maxResult);
-            int overcome1=(int)Math.rint(minResult);
             weatherD.mTemperature=Integer.toString(roundedValue);
-            weatherD.maxTemp=Integer.toString(overcome);
-            weatherD.minTemp=Integer.toString(overcome1);
+            //Sunrise
+            Long sunriseResult=jsonObject.getJSONObject("sys").getLong("sunrise");
+            SimpleDateFormat srf = new SimpleDateFormat("HH:mm:ss");
+            weatherD.sunRise = srf.format(new java.util.Date (sunriseResult*1000));
+            //Sunset
+            Long sunsetResult=jsonObject.getJSONObject("sys").getLong("sunset");
+            SimpleDateFormat ssf = new SimpleDateFormat("HH:mm:ss");
+            weatherD.sunSet = ssf.format(new java.util.Date (sunsetResult*1000));
+            //Weather icon
+            String icon = jsonObject.getJSONArray("weather").getJSONObject(0).getString("icon");
+            weatherD.micon=updateWeathericon(icon);
+            //Windspeed
+            weatherD.windSpeed=jsonObject.getJSONObject("wind").getDouble("speed");
+            //Main(clear,clouds,thunderstorm,etc)
+            weatherD.maint=jsonObject.getJSONArray("weather").getJSONObject(0).getString("main");
+            //Humidity
+            int hum = jsonObject.getJSONObject("main").getInt("humidity");
+            weatherD.mHumidity = Integer.toString(hum);
+            //Pressure
+            int press = jsonObject.getJSONObject("main").getInt("pressure");
+            weatherD.mPressure = Integer.toString(press);
+            //Clouds
+            int cloudss = jsonObject.getJSONObject("clouds").getInt("all");
+            weatherD.mClouds = Integer.toString(cloudss);
             return weatherD;
         }
 
@@ -36,17 +61,67 @@ public class weatherData {
 
 
     }
+
+    private static String updateWeathericon(String j){
+        if (j.equals("01d")){
+            return "d_clear";
+        }
+
+        else if (j.equals("11d")){
+            return "d_thunder";
+        }
+        else if (j.equals("09d") || j.equals("10d")){
+            return "d_rain";
+        }
+        else if (j.equals("13d")){
+            return "d_snow";
+        }
+        else if (j.equals("02d") || j.equals("03d") || j.equals("04d")){
+            return "d_clouds";
+        }
+        else if (j.equals("01n")){
+            return "n_clear";
+        }
+        else if (j.equals("11n")){
+            return "n_thunder";
+        }
+        else if (j.equals("09n") || j.equals("10n")){
+            return "n_rain";
+        }
+        else if (j.equals("13n")){
+            return "n_snow";
+        }
+        else if (j.equals("02n") || j.equals("03n") || j.equals("04n")){
+            return "n_clouds";
+        }
+        return "d_clear";
+    }
     public String getmTemperature() {
         return mTemperature+"°C";
     }
-    public String getMaxTemp() {
-        return maxTemp+"°C ↑";
+    public String getSunRise() {
+        return sunRise;
+    }
+    public String getSunSet() {
+        return sunSet;
     }
 
-    public String getMinTemp() {
-        return minTemp + "°C ↓";
+    public String getMaint() {
+        return maint;
     }
     public String getwindSpeed() {
         return windSpeed + "m/s";
+    }
+    public String getMicon(){
+        return micon;
+    }
+    public String getMhumidity(){
+        return mHumidity;
+    }
+    public String getmClouds(){
+        return mClouds;
+    }
+    public String getmPressure(){
+        return mPressure;
     }
 }
